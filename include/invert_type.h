ed459778a156b504532b5870a9abf12242e06f7e
zilong.whu <zilong.whu@gmail.com>
2014-03-07 04:20:52 +0000

    update

--- include/invert_type.h
+++ include/invert_type.h
@@ -17,6 +17,7 @@
 #ifndef __AGILE_SE_INVERT_TYPE_H__
 #define __AGILE_SE_INVERT_TYPE_H__
 
+#include <openssl/md5.h>
 #include "invert_parser.h"
 
 struct InvertType
@@ -27,26 +28,36 @@ struct InvertType
     InvertParser *parser;
 };
 
-struct InvTypes
+struct InvertTypes
 {
-    struct InvType types[UINT8_MAX];                  /*  invert type is offset */
+    struct InvertType types[256];               /*  invert type is offset */
 
-    InvTypes()
+    InvertTypes()
     {
         ::memset(types, 0, sizeof types);
         for (size_t i = 0; i < sizeof(types)/sizeof(types[0]); ++i)
         {
-            types[i].type = -1;
+            types[i].type = 0xFF;
         }
     }
     bool is_valid_type(uint8_t type) const
     {
         return types[type].type == type;
     }
-    uint64_t get_sign(const char *keystr, int8_t type) const
+    uint64_t get_sign(const char *keystr, uint8_t type) const
     {
-        /* not implemented yet */
-        return 0;
+        char buffer[256];
+
+        ::snprintf(buffer, sizeof buffer, "%s%s", types[type].prefix, keystr);
+        buffer[sizeof(buffer) - 1] = '\0';
+
+        unsigned int md5res[4];
+        MD5((unsigned char*)buffer,(unsigned int)::strlen(buffer),(unsigned char*)md5res);
+
+        uint64_t sign = md5res[0]+md5res[1];
+        sign <<= 32;
+        sign |= md5res[2]+md5res[3];
+        return sign;
     }
 };
 
