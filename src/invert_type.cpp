103e84f68886bdcff5a9de6fc1958180d108bc66
zilong.whu <zilong.whu@gmail.com>
2014-03-07 04:31:53 +0000

    update

---
+++ src/invert_type.cpp
@@ -0,0 +1,24 @@
+// =====================================================================================
+//
+//       Filename:  invert_type.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  03/07/2014 12:24:48 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Zilong.Zhu (), zilong.whu@gmail.com
+//        Company:  edu.whu
+//
+// =====================================================================================
+
+#include "log.h"
+#include "configure.h"
+#include "invert_type.h"
+
+int InvertTypes::init(const char *path, const char *file)
+{
+    return 0;
+}
