1fbb3d73ea24b8a9afd899ed1c91be99219384f6
zilong.whu@gmail.com <zilong.whu@gmail.com@5de44f08-11a8-056b-7ee5-35afb8e6aba2>
2014-05-05 08:43:56 +0000

    fix skiplist rand level func && add builder

--- trunk/include/invert_index.h
+++ trunk/include/invert_index.h
@@ -76,6 +76,7 @@ class InvertIndex
         bool insert(const char *keystr, uint8_t type, int32_t docid, const std::string &json);
         bool insert(const char *keystr, uint8_t type, int32_t docid, cJSON *json);
         bool remove(const char *keystr, uint8_t type, int32_t docid);
+        bool remove(uint64_t sign, int32_t docid);
 
         void recycle()
         {
