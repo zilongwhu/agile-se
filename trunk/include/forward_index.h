d038dde209b903f4d38adca554f116cc1c072a99
zilong.whu@gmail.com <zilong.whu@gmail.com@5de44f08-11a8-056b-7ee5-35afb8e6aba2>
2014-05-29 07:31:46 +0000

    add forward iterators

--- trunk/include/forward_index.h
+++ trunk/include/forward_index.h
@@ -31,8 +31,87 @@
 
 class ForwardIndex
 {
+    private:
+        typedef TDelayPool<VMemoryPool> Pool;
+        typedef Pool::vaddr_t vaddr_t;
+        typedef HashTable<int32_t, vaddr_t> Hash;
+        typedef Hash::ObjectPool NodePool;
     public:
         enum { INT_TYPE = 0, FLOAT_TYPE, SELF_DEFINE_TYPE };
+
+        class FieldIterator
+        {
+            public:
+                FieldIterator(const ForwardIndex *idx, void *info)
+                    : m_idx(idx)
+                {
+                    m_pos = 0;
+                    m_info = info;
+                }
+
+                bool next(std::string &field_name, int &type, int *iv, float *fv, void **pv)
+                {
+                    *iv = 0;
+                    *fv = 0;
+                    *pv = NULL;
+                    if (m_pos >= m_idx->m_field_names.size())
+                    {
+                        return false;
+                    }
+                    field_name = m_idx->m_field_names[m_pos].second;
+                    __gnu_cxx::hash_map<std::string, FieldDes>::const_iterator it = m_idx->m_fields.find(field_name);
+                    if (it != m_idx->m_fields.end())
+                    {
+                        if (it->second.type == INT_TYPE)
+                        {
+                            *iv = ((int *)m_info)[it->second.array_offset];
+                        }
+                        else if (it->second.type == FLOAT_TYPE)
+                        {
+                            *fv = ((float *)m_info)[it->second.array_offset];
+                        }
+                        else
+                        {
+                            *pv = ((void **)m_info)[it->second.array_offset];
+                        }
+                    }
+                    ++m_pos;
+                    return true;
+                }
+            private:
+                size_t m_pos;
+                void *m_info;
+                const ForwardIndex *m_idx;
+        };
+
+        class iterator
+        {
+            public:
+                iterator(const ForwardIndex *idx)
+                    : m_idx(idx), m_it(m_idx->m_dict->begin())
+                { }
+
+                bool next(int32_t *docid, FieldIterator *it)
+                {
+                    if (!m_it)
+                    {
+                        return false;
+                    }
+                    if (docid)
+                    {
+                        *docid = m_it.key();
+                    }
+                    if (it)
+                    {
+                        *it = m_idx->get_field_iterator(m_idx->m_pool.addr(m_it.value()));
+                    }
+                    ++m_it;
+                    return true;
+                }
+            private:
+                const ForwardIndex *m_idx;
+                Hash::iterator m_it;
+        };
 
         struct FieldDes
         {
@@ -45,11 +124,6 @@ class ForwardIndex
                 const google::protobuf::Message *default_message;
             };
         };
-    private:
-        typedef TDelayPool<VMemoryPool> Pool;
-        typedef Pool::vaddr_t vaddr_t;
-        typedef HashTable<int32_t, vaddr_t> Hash;
-        typedef Hash::ObjectPool NodePool;
     private:
         ForwardIndex(const ForwardIndex &);
         ForwardIndex &operator =(const ForwardIndex &);
@@ -82,6 +156,12 @@ class ForwardIndex
 
         bool load(const char *dir);
         bool dump(const char *dir) const;
+
+        FieldIterator get_field_iterator(void *info) const
+        {
+            return FieldIterator(this, info);
+        }
+        iterator begin() const { return iterator(this); }
     private:
         struct cleanup_data_t
         {
@@ -114,6 +194,7 @@ class ForwardIndex
         Hash *m_dict;
 
         __gnu_cxx::hash_map<std::string, FieldDes> m_fields;
+        std::vector<std::pair<uint32_t, std::string> > m_field_names;
         std::vector<std::pair<uint32_t, double> > m_default_values;
         std::vector<const google::protobuf::Message *> m_default_messages;
         size_t m_info_size;
