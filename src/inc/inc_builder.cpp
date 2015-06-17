a5d0d9e58956450a883dbc5fd14dd188d23793a4
zilongwhu <zilong_whu@163.com>
2015-06-17 23:39:10 +0800

    add source files<mass>

---
+++ src/inc/inc_builder.cpp
@@ -0,0 +1,248 @@
+#include <stddef.h>
+#include "inc/inc_utils.h"
+#include "inc/inc_builder.h"
+#include "utils/file_watcher.h"
+#include "utils/meminfo.h"
+
+namespace inc
+{
+    cJSON *parse_forward_json(const std::string &json, std::vector<forward_data_t> &values)
+    {
+        cJSON *object = cJSON_Parse(json.c_str());
+        if (NULL == object)
+        {
+            P_WARNING("failed to parse json<%s>", json.c_str());
+            return NULL;
+        }
+        forward_data_t data;
+        if (cJSON_Object != object->type)
+        {
+            P_WARNING("invalid json<%s>, must be an Object", json.c_str());
+            goto FAIL;
+        }
+        values.clear();
+        for (cJSON *c = object->child; c; c = c->next)
+        {
+            if (cJSON_Array != c->type) {
+                if (data.build(c)) {
+                    values.push_back(data);
+                } else {
+                    goto FAIL;
+                }
+            } else {
+                P_WARNING("invalid json<%s>, value of <%s> cannot be an Array",
+                        json.c_str(), c->string);
+                goto FAIL;
+            }
+        }
+        return object;
+    FAIL:
+        values.clear();
+        cJSON_Delete(object);
+        return NULL;
+    }
+
+    cJSON *parse_invert_json(const std::string &json, std::vector<invert_data_t> &values)
+    {
+        cJSON *cjson = cJSON_Parse(json.c_str());
+        if (NULL == cjson)
+        {
+            P_WARNING("failed to parse json<%s>", json.c_str());
+            return NULL;
+        }
+        invert_data_t data;
+        if (cJSON_Object != cjson->type)
+        {
+            P_WARNING("invalid json<%s>, must be an Object", json.c_str());
+            goto FAIL;
+        }
+        values.clear();
+        for (cJSON *c = object->child; c; c = c->next)
+        {
+            int type = -1;
+            if (!parseInt32(c->string, type) || type >= UINT8_MAX || type < 0)
+            {
+                P_WARNING("invalid json<%s>, invert type<%s> must be an uint8_t[0, 0xFF)",
+                        json.c_str(), c->string);
+                goto FAIL;
+            }
+            if (cJSON_Object != c->type)
+            {
+                P_WARNING("invalid json<%s>, inverts[%d] must be an Object", json.c_str(), (int)type);
+                goto FAIL;
+            }
+            for (cJSON *cc = c->child; cc; cc = cc->next)
+            {
+                if (!data.build(type, value))
+                {
+                    goto FAIL;
+                }
+                values.push_back(data);
+            }
+        }
+        return cjson;
+FAIL:
+        values.clear();
+        cJSON_Delete(cjson);
+        return NULL;
+    }
+
+    int inc_build_index(inc_builder_t *args)
+    {
+        Index &idx = *args->idx;
+        IncReader &reader = idx.m_inc_reader;
+
+        const size_t log_buffer_length = 4096;
+        char *log_buffer = new char[log_buffer_length];
+    
+        char *line = reader._line_buf;
+        uint32_t now = g_now_time;
+        uint32_t delete_count = 0;
+        uint32_t update_invert_count = 0;
+        uint32_t update_forward_count = 0;
+    
+        std::vector<forward_data_t> fields;
+        std::vector<invert_data_t> inverts;
+        while (1)
+        {
+            if (now != g_now_time)
+            {
+                P_MYLOG("processe stats: doc_num[%d], update[forward=%u, invert=%u], delete[%u], in[%u ~ %u]",
+                        (int)idx.docs_num(), update_forward_count, update_invert_count, delete_count,
+                        now, g_now_time);
+    
+                idx.recycle();
+
+                if (args->timer)
+                {
+                    args->timer(args);
+                }
+
+                idx.try2merge();
+                idx.try2dump();
+                idx.try_exc_cmd();
+                idx.try_print_meta();
+                idx.try_print_list();
+
+                long check_count = 0;
+                while (1)
+                {
+                    Meminfo mi;
+                    if (mi.free() + mi.buffers() + mi.cached() <= long(mi.total() * 0.1))
+                    {
+                        P_WARNING("memory panic, free: %ld KB, buffers: %ld KB, cached: %ld KB, total: %ld KB",
+                                mi.free(), mi.buffers(), mi.cached(), mi.total());
+                        if (check_count++ % 180 == 0) { /* log fatal every 3 minutes */
+                            P_FATAL("incthread is suspending now");
+                        }
+                        ::sleep(1); /* sleep 1 second */
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+    
+                delete_count = 0;
+                update_invert_count = 0;
+                update_forward_count = 0;
+                now = g_now_time;
+            }
+            int ret = reader.next();
+            if (0 == ret) {
+                if (idx.is_base_mode())
+                {
+                    P_WARNING("meeting end of base file");
+                    idx.dump();
+
+                    delete [] log_buffer;
+                    return 0;
+                }
+                ::usleep(1000);
+            } else if (1 == ret) {
+                std::vector<std::string> columns;
+                split(line, "\t", columns);
+                if (columns.size() < 4)
+                {
+                    P_MYLOG("must has [eventid, optype, level, oid]");
+                    continue;
+                }
+                int32_t optype = ::strtol(columns[1].c_str(), NULL, 10);
+                int32_t level = ::strtol(columns[2].c_str(), NULL, 10);
+                int32_t oid = ::strtoul(columns[3].c_str(), NULL, 10);
+                if (oid % reader.total_partition() != reader.current_partition())
+                {
+                    continue;
+                }
+                fields.clear();
+                inverts.clear();
+                ForwardIndex::internal_ids_t ids;
+                switch (level)
+                {
+                    case FORWARD_LEVEL:
+                        if (OP_UPDATE == optype)
+                        {
+                            cJSON *json = NULL;
+                            if (columns.size() < 5) {
+                                P_MYLOG("forward update need at least 5 fields");
+                            } else if (NULL == (json = parse_forward_json(columns[4], fields))) {
+                                P_MYLOG("failed to parse forward json");
+                            } else if (!idx.forward_update(oid, fields, &ids)) {
+                                P_MYLOG("failed to update forward index");
+                            } else if (!idx.update_docid(ids.old_id, ids.new_id)) {
+                                P_MYLOG("failed to change docid from %d to %d, oid=%d",
+                                        ids.old_id, ids.new_id, oid);
+                            } else {
+                                P_TRACE("update forward ok, oid=%d", oid);
+                                ++update_forward_count;
+                            }
+                            cJSON_Delete(json);
+                        }
+                        break;
+                    case INVERT_LEVEL:
+                        if (OP_DELETE == optype)
+                        {
+                            int32_t old_id = -1;
+                            if (idx.forward_remove(oid, &old_id)) /* remove & get old internal id */
+                            {
+                                idx.invert_remove(old_id);
+                            }
+                            P_TRACE("delete ok, oid=%d", oid);
+                            ++delete_count;
+                        }
+                        else if (OP_UPDATE == optype)
+                        {
+                            cJSON *fjson = NULL;
+                            cJSON *ijson = NULL;
+                            if (columns.size() < 6) {
+                                P_MYLOG("invert update need at least 6 fields");
+                            } else if (NULL == (fjson = parse_forward_json(columns[4], fields)) {
+                                P_MYLOG("failed to parse forward json");
+                            } else if (NULL == (ijson = parse_invert_json(columns[5], inverts)) {
+                                P_MYLOG("failed to parse invert json");
+                            } else if (!idx.forward_update(oid, fields, &ids)) {
+                                P_MYLOG("failed to update forward index");
+                            } else {
+                                idx.invert_remove(ids.old_id);
+                                for (size_t i = 0; i < inverts.size(); ++i)
+                                {
+                                    idx.invert_insert(inverts[i].key, inverts[i].type,
+                                        ids.new_id, inverts[i].value);
+                                }
+                                P_TRACE("update ok, oid=%d, id=%d", oid, ids.new_id);
+                                ++update_invert_count;
+                            }
+                            cJSON_Delete(fjson);
+                            cJSON_Delete(ijson);
+                        }
+                        break;
+                };
+            } else if (ret < 0) {
+                P_FATAL("disk file error");
+                break;
+            }
+        }
+        delete [] log_buffer;
+        return -1;
+    }
+}
---
+++ src/inc/inc_reader.cpp
@@ -0,0 +1,273 @@
+#include	<errno.h>
+#include	<fstream>
+#include	"inc/inc_utils.h"
+#include	"inc/inc_reader.h"
+#include	"utils/log.h"
+#include	"utils/fast_timer.h"
+#include	"configure.h"
+
+namespace inc
+{
+    int IncReader::init(const char *path, const char *meta)
+    {
+        std::string fp;
+        TRY
+        {
+            Config conf(path, meta);
+            if (conf.parse() != 0)
+            {
+                P_WARNING("failed to parse [%s][%s]", path, meta);
+                return -1;
+            }
+            fp = conf["inc_file_prefix"];
+            bool ret = true;
+            ret = ret && parseUInt32(conf["file_no"].c_str(), _file_no);
+            ret = ret && parseUInt32(conf["line_no"].c_str(), _line_no);
+            ret = ret && parseUInt32(conf["max_line_no"].c_str(), _max_line_no);
+            ret = ret && parseUInt32(conf["partition_cur"].c_str(), _partition_cur);
+            ret = ret && parseUInt32(conf["partition_num"].c_str(), _partition_num);
+            ret = ret && parseUInt32(conf["base_mode"].c_str(), _base_mode);
+            _inc_name = conf["inc_name"];
+
+            if (!ret)
+            {
+                P_WARNING("failed to load parameters");
+                return -1;
+            }
+            if (_partition_cur >= _partition_num || _partition_num == 0)
+            {
+                P_WARNING("invalid partition info[%u:%u]", _partition_cur, _partition_num);
+                return -1;
+            }
+        }
+        WARING_CATCH_EXEC(return -1, "failed to load conf from [%s] [%s]", path, meta);
+        if (_fp)
+        {
+            ::fclose(_fp);
+            _fp = NULL;
+        }
+        ::snprintf(_filepath, sizeof(_filepath) - sizeof(".xxxxxxxxxx"), "%s", fp.c_str());
+        _path_len = ::strlen(_filepath);
+    
+        ::sprintf(_filepath + _path_len, ".%d", _file_no);
+        _fp = ::fopen(_filepath, "r");
+    
+        if (NULL == _fp)
+        {
+            P_FATAL("failed to open file[%s].", _filepath);
+            return -1;
+        }
+        if (_line_no > 0)
+        {
+            int ret;
+            for (uint32_t i = 0; i < _line_no; ++i)
+            {
+                ret = readline(_fp, _line_buf, sizeof _line_buf, true);
+                if (ret <= 0)
+                {
+                    if (ret < 0)
+                    {
+                        P_FATAL("read line[%u] from file[%s] error.", i + 1, _filepath);
+                    }
+                    else
+                    {
+                        P_FATAL("only read %u lines from file[%s], which should have %u lines.", i, _filepath, _line_no);
+                    }
+                    goto ERROR;
+                }
+                else if (ret >= (int)sizeof _line_buf)
+                {
+                    P_WARNING("line[%u] too long in file[%s].", i + 1, _filepath);
+                }
+            }
+            char *end = ::strchr(_line_buf, '\t');
+            if (end)
+            {
+                *end = '\0';
+            }
+        }
+        P_WARNING("init inc_reader ok, filepath[%s] fileno[%u] lineno[%u] max_lineno[%u] partition[%u:%u]%s.",
+                fp.c_str(), _file_no, _line_no, _max_line_no, _partition_cur, _partition_num,
+                _base_mode ? " base" : "");
+        return 0;
+ERROR:
+        ::fclose(_fp);
+        _fp = NULL;
+        return -1;
+    }
+
+    int IncReader::dumpMeta(const char *path, const char *meta)
+    {
+        std::string p(path);
+        p += "/" + std::string(meta);
+        std::ofstream fout(p.c_str());
+        fout << "inc_file_prefix: " << std::string(_filepath, _path_len) << std::endl;
+        fout << "file_no: " << _file_no << std::endl;
+        fout << "line_no: " << _line_no << std::endl;
+        fout << "max_line_no: " << _max_line_no << std::endl;
+        fout << "partition_cur: " << _partition_cur << std::endl;
+        fout << "partition_num: " << _partition_num << std::endl;
+        fout.close();
+        return fout ? 0 : -1;
+    }
+    
+    int IncReader::next()
+    {
+        if (NULL == _fp)
+        {
+            P_FATAL("_fp is NULL");
+            return -1;
+        }
+        if (_line_no >= _max_line_no)
+        {
+            ::sprintf(_filepath + _path_len, ".%d", _file_no + 1);
+            FILE *fp = ::fopen(_filepath, "r");
+    
+            if (NULL == fp)
+            {
+                P_WARNING("failed to open file[%s].", _filepath);
+                return 0; /* try again at some later time */
+            }
+            ::fclose(_fp);
+    
+            _fp = fp;
+            ++_file_no;
+            _line_no = 0;
+        }
+        int ret = readline(_fp, _line_buf, sizeof _line_buf, true);
+        if (ret < 0)
+        {
+            P_FATAL("error occured when reading file[%s], line_no[%u].", _filepath, _line_no);
+            return -1;
+        }
+        else if (0 == ret)
+        {
+            return 0; /* try again at some later time */
+        }
+        else if (ret >= (int)sizeof _line_buf)
+        {
+            P_WARNING("too long line at file[%s], line_no[%u], length = %d.", _filepath, _line_no, ret);
+        }
+        ++_line_no;
+        if (!_base_mode)
+        {
+            _eventid = ::strtoul(_line_buf, NULL, 10);
+        }
+        return 1;
+    }
+    
+    int IncReader::snapshot()
+    {
+        _ss_file_no = _file_no;
+        _ss_line_no = _line_no;
+        if (NULL == _fp)
+        {
+            P_WARNING("_fp is NULL, failed to make snapshot.");
+            return -1;
+        }
+        _ss_pos = ::ftello(_fp);
+        if (-1 == _ss_pos)
+        {
+            P_WARNING("ftello return -1, errno[%d].", errno);
+            return -1;
+        }
+        _snapshot = true;
+        return 0;
+    }
+    
+    int IncReader::rollback()
+    {
+        if (_snapshot)
+        {
+            if (_ss_file_no == _file_no)
+            {
+                if (::fseeko(_fp, _ss_pos, SEEK_SET) < 0)
+                {
+                    P_WARNING("failed to seek file[%s], line no[%u]", _filepath, _ss_line_no);
+                    return -1;
+                }
+            }
+            else
+            {
+                ::sprintf(_filepath + _path_len, ".%d", _ss_file_no);
+                FILE *fp = ::fopen(_filepath, "r");
+    
+                if (NULL == fp)
+                {
+                    P_WARNING("failed to open file[%s].", _filepath);
+                    ::sprintf(_filepath + _path_len, ".%d", _file_no);
+                    return -1;
+                }
+                if (::fseeko(fp, _ss_pos, SEEK_SET) < 0)
+                {
+                    P_WARNING("failed to seek file[%s], line no[%u]", _filepath, _ss_line_no);
+                    ::sprintf(_filepath + _path_len, ".%d", _file_no);
+                    ::fclose(fp);
+                    return -1;
+                }
+    
+                ::fclose(_fp);
+                _fp = fp;
+            }
+            _file_no = _ss_file_no;
+            _line_no = _ss_line_no;
+            _snapshot = false;
+        }
+        return 0;
+    }
+    
+    int IncReader::collect_lines(int max_lines, int max_ms)
+    {
+        int ret;
+        int cnt;
+        FastTimer timer;
+    
+        timer.start();
+        for (cnt = 0; cnt < max_lines; )
+        {
+            ret = this->next();
+            if (1 == ret)
+            {
+                ++cnt;
+                continue;
+            }
+            else if (0 == ret)
+            {
+                timer.stop();
+                if (timer.timeInMs() > max_ms)
+                { /* wait too long */
+                    P_DEBUG("wait too long, collect only %d lines.", cnt);
+                    break;
+                }
+                ::usleep(500*1000); /* 500ms */
+                continue;
+            }
+            else
+            {
+                P_FATAL("failed to read a complete line.");
+                break;
+            }
+        }
+        timer.stop();
+        if (cnt > 0)
+        {
+            P_WARNING("collect %d lines in %d ms, start to process.", cnt, (int)timer.timeInMs());
+        }
+        return cnt;
+    }
+    
+    void IncReader::log(const char *str)
+    {
+        if (str)
+        {
+            if (_base_mode)
+            {
+                P_WARNING("file_no[%u] line_no[%u] type[%s]: %s", _file_no, _line_no, _inc_name.c_str(), str);
+            }
+            else
+            {
+                P_WARNING("eventid[%lu] file_no[%u] line_no[%u] type[%s]: %s", _eventid, _file_no, _line_no, _inc_name.c_str(), str);
+            }
+        }
+    }
+}
