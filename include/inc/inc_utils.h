ee2dd9590a2219ac2c9294dee98b10ad2ec8b0ab
zilongwhu <zilong_whu@163.com>
2015-06-18 13:40:36 +0800

    update

--- include/inc/inc_utils.h
+++ include/inc/inc_utils.h
@@ -13,6 +13,33 @@ namespace inc
     bool parseInt64(const char *str, int64_t &val, bool strict = false);
     bool parseUInt64(const char *str, uint64_t &val, bool strict = false);
     bool parseFloat(const char *str, float &val, bool strict = false);
+    bool parseDouble(const char *str, double &val, bool strict = false);
+
+    inline bool parseInt32(const std::string &str, int32_t &val, bool strict = false)
+    {
+        return parseInt32(str.c_str(), val, strict);
+    }
+    inline bool parseUInt32(const std::string &str, uint32_t &val, bool strict = false)
+    {
+        return parseUInt32(str.c_str(), val, strict);
+    }
+    inline bool parseInt64(const std::string &str, int64_t &val, bool strict = false)
+    {
+        return parseInt64(str.c_str(), val, strict);
+    }
+    inline bool parseUInt64(const std::string &str, uint64_t &val, bool strict = false)
+    {
+        return parseUInt64(str.c_str(), val, strict);
+    }
+    inline bool parseFloat(const std::string &str, float &val, bool strict = false)
+    {
+        return parseFloat(str.c_str(), val, strict);
+    }
+    inline bool parseDouble(const std::string &str, double &val, bool strict = false)
+    {
+        return parseDouble(str.c_str(), val, strict);
+    }
+
     int readline(FILE *fp, char *line, int num, bool ignoreRest);
     void split(const std::string& s, const std::string& delim, std::vector<std::string> &elems);
 
